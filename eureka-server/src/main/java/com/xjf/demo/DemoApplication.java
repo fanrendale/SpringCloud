c008bc409c4524a36870616016b6b59e391bfd14
fanrendale <1053314919@qq.com>
2020-01-21 17:19:04 +0800

    Eureka注册中心

---
+++ eureka-server/src/main/java/com/xjf/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.xjf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 开启Eureka注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
