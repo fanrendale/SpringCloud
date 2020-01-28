77f2ef09aa04243c64dbb7aa8b1d0f09ebccf6a2
fanrendale <1053314919@qq.com>
2020-01-28 21:28:57 +0800

    zuul的简单使用、集成eureka使用服务名称调用服务

---
+++ zuul-demo/src/main/java/com/xjf/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.xjf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 开区路由代理功能
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
