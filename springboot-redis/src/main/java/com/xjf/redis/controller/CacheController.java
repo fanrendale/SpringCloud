ee2cefcfc8de897623b0a431ea02f2465ceda0f2
fanrendale <1053314919@qq.com>
2020-02-11 15:07:56 +0800

    Redis : 缓存雪崩

--- springboot-redis/src/main/java/com/xjf/redis/controller/CacheController.java
+++ springboot-redis/src/main/java/com/xjf/redis/controller/CacheController.java
@@ -4,9 +4,13 @@ import com.xjf.redis.entity.Person;
 import com.xjf.redis.service.CacheService;
 import com.xjf.redis.service.Closure;
 import com.xjf.redis.service.PersonService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 自定义缓存工具的使用
@@ -21,7 +25,45 @@ public class CacheController {
     private CacheService cacheService;
 
     @Autowired
-    public PersonService personService;
+    private PersonService personService;
+
+    /**
+     * RedissonClient 客户端
+     */
+    @Autowired
+    private RedissonClient redissonClient;
+
+    /**
+     * 测试 Redis 的分布式锁
+     * @return
+     */
+    @GetMapping("/redisson")
+    public String redisson() throws InterruptedException {
+        // 获取指定名称的锁
+        RLock lock = redissonClient.getLock("anyLock");
+
+        //支持过期解锁。 10s 后自动解锁。 无需调用 unlock 方法手动解锁
+        lock.lock(30, TimeUnit.SECONDS);
+        System.out.println("AAAAAAA");
+
+//        TimeUnit.MINUTES.sleep(1);
+//        lock.unlock();
+
+        return "AAAAAAA";
+    }
+
+    @GetMapping("/redisson2")
+    public String redisson2(){
+        // 获取指定名称的锁
+        RLock lock = redissonClient.getLock("anyLock");
+
+        //支持过期解锁。 10s 后自动解锁。 无需调用 unlock 方法手动解锁
+        lock.lock(30, TimeUnit.SECONDS);
+        System.out.println("BBBBBBBBB");
+//        lock.unlock();
+
+        return "BBBBBBBBB";
+    }
 
 
     @GetMapping("/cache/get")
