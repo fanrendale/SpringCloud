68665ca78ebb4f6e434f66d10b1ecf5cf54955a9
fanrendale <1053314919@qq.com>
2020-02-16 10:40:29 +0800

    Elastic-Job: 分片任务执行

--- elastic-job-demo/src/main/java/com/xjf/demo/job/MySimpleJob.java
+++ elastic-job-demo/src/main/java/com/xjf/demo/job/MySimpleJob.java
@@ -5,6 +5,7 @@ import com.dangdang.ddframe.job.api.simple.SimpleJob;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 自定义实现任务
@@ -15,7 +16,25 @@ import java.time.format.DateTimeFormatter;
 public class MySimpleJob implements SimpleJob {
     @Override
     public void execute(ShardingContext shardingContext) {
-        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        System.err.println("开始执行简单任务：" + time);
+        /*String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.err.println("开始执行简单任务：" + time);*/
+
+        // 测试数据分片处理
+        String shardingParameter = shardingContext.getShardingParameter();
+        System.err.println("分片参数：" + shardingParameter);
+        Integer value = Integer.valueOf(shardingParameter);
+
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == value){
+                System.err.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " : 开始执行简单任务 " + i);
+
+                // 睡眠 5s
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
